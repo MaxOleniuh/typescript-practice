bc7310c045871c5968590968731bbb2374d32181
oleniuh <maxim.oleniukh@gmail.com>
2023-06-17 08:21:02 +0100

    hook useRef() in TypeScript

--- src/components/EventsExample.tsx
+++ src/components/EventsExample.tsx
@@ -4,11 +4,11 @@ const EventsExample: FC = () => {
     const [value, setValue] = useState<string>('');
     const [isDrag, setIsDrag] = useState<boolean>(false);
     const inputRef = useRef<HTMLInputElement>(null);
-    const eventHandler = (e:React.ChangeEvent<HTMLInputElement>) => {
+    const changeHandler = (e:React.ChangeEvent<HTMLInputElement>) => {
         setValue(e.target.value)
     }
     const clickHandler = (e:React.MouseEvent<HTMLButtonElement>) => {
-        console.log(value)
+        console.log(inputRef.current?.value)
     }
     const dragHandler = (e: React.DragEvent<HTMLDivElement>) => {
         console.log('Drag')
@@ -28,8 +28,8 @@ const EventsExample: FC = () => {
     }
   return (
       <div>
-          <input value={value} type="text" onChange={eventHandler} placeholder='Controllable' />
-          <input value={value} type="text" onChange={eventHandler} placeholder='Uncontrollable'/>
+          <input value={value} type="text" onChange={changeHandler} placeholder='Controllable' />
+          <input ref={inputRef} placeholder='Uncontrollable'/>
           <button onClick={clickHandler}>Text</button>
           <div onDrag={dragHandler} draggable style={{ width: '200px', height: '200px', background: 'orange' }}></div>
           <div onDrop={dropHandler} onDragLeave={leaveHandler} onDragOver={dragWithPreventHandler}
